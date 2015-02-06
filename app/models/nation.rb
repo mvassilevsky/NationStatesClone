ed03a7dbd63a53dba9509d39c0373744e0059fea
Michael Vassilevsky <mvassilevsky@gmail.com>
2015-02-06 09:39:38 -0800

    finished government type descriptions

--- app/models/nation.rb
+++ app/models/nation.rb
@@ -262,26 +262,83 @@ class Nation < ActiveRecord::Base
       "are known throughout the region for their efficiency and work ethic," +
       " as well as their general suspicion of leisure"
     when "Corporate Police State"
+      "are ruled with an iron fist by the corrupt, dictatorial government," +
+      " which oppresses anyone who isn't on the board of a Fortune 500" +
+      " company. Large corporations tend to be above the law, and use their" +
+      " financial clout to gain ever-increasing government benefits at the" +
+      " expense of the poor and unemployed"
     when "Right-Wing Utopia"
+      "are free to succeed or fail in life on their own merits; the" +
+      " successful tend to enjoy an opulent (but moralistic) lifestyle, while" +
+      " the failures can be seen crowding out most jails"
     when "Corporatist Democracy"
+      "enjoy frequent elections, which are uniformly corrupted by" +
+      " big-spending corporations buying politicians who best suit their" +
+      " interests"
     when "Left-Wing Autocracy"
+      "are ruled with an iron fist by the autocratic government, which" +
+      " ensures that no-one outside the party gets too rich. In their" +
+      " personal lives, however, citizens are relatively unoppressed; it" +
+      " remains to be seen whether this is because the government genuinely" +
+      " cares about its people, or if it hasn't gotten around to stamping out" +
+      " civil rights yet"
     when "Bureaucratic Socialism"
+      "are fiercely patriotic and enjoy great social equality; they tend to" +
+      " view other, more capitalist countries as somewhat immoral and corrupt"
     when "Ultra-Electoral Socialism"
+      "love a good election, and the government gives them plenty of them." +
+      " Universities tend to be full of students debating the merits of" +
+      " various civil and political rights, while businesses are tightly" +
+      " regulated and the wealthy viewed with suspicion"
     when "Paternalistic Dictatorship"
+      "are ruled by a mostly-benevolent dictator, who grants the populace the" +
+      " freedom to live their own lives but watches carefully for anyone to" +
+      " slip up"
     when "Centrist Democracy"
+      "have some civil rights, but not too many, enjoy the freedom to spend" +
+      " their money however they like, to a point, and take part in free and" +
+      " open elections, although not too often"
     when "Ultra-Electoral State"
+      "enjoy a sensible mix of personal and economic freedoms, while the" +
+      " political process is open and the people's right to vote held" +
+      " sacrosanct"
     when "Neocameralist State"
-    when "Free-Market Bureaucracy"
+      "are ruled by a state that operates like a business, which leaves" +
+      " commerce largely unregulated but stamps out all talk of elections"
     when "Market Democracy"
+      "are either ruled by a small, efficient government or a conglomerate of" +
+      " multinational corporations; it's difficult to tell which"
     when "Liberal Socialist Autocracy"
+      "are ruled with an iron fist by the socialist government, which grants" +
+      " its people the freedom to do whatever they like so long as it doesn't" +
+      " involve getting richer than anybody else or challenging the government"
     when "Liberal Socialism"
+      "enjoy extensive civil rights and enjoy a level of social equality free" +
+      " from the usual accompanying government corruption"
     when "Left-Wing Utopia"
+      "are free to do what they want with their own bodies, and vote for" +
+      " whomever they like in elections; if they go into business, however," +
+      " they are regulated to within an inch of their lives"
     when "Permissive Dictatorship"
+      "are proud of their wide-ranging civil freedoms, and those who aren't" +
+      " tend to be dragged off the streets by men in dark suits and hustled" +
+      " into cars with tinted windows"
     when "Progressive Welfare State"
+      "enjoy extensive civil freedoms, particularly in social issues, while" +
+      " business tends to be more regulated"
     when "Individualist Progressive State"
+      "hold their civil and political rights very dear, although the wealthy" +
+      " and those in business tend to be viewed with suspicion"
     when "Libertarian Dictatorship"
+      "enjoy great individual freedoms in everything except elections, which," +
+      " where they exist at all, are populist shams for a dictatorial" +
+      " government that has no intention of ever giving up power"
     when "Democratic Night-Watchman State"
+      "enjoy a large degree of social and economic freedom, though" +
+      " experiencing a significant degree of inequality"
     when "Anarchy"
+      "live in a state of perpetual fear, as a complete breakdown of social" +
+      " order has led to the rise of order through biker gangs"
     end
   end
 
