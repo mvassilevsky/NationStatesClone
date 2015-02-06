fb94d7f93c181848fec6f5563c9905809b044ae8
Michael Vassilevsky <mvassilevsky@gmail.com>
2015-02-06 10:29:12 -0800

    added newrelic.yaml

--- app/models/nation.rb
+++ app/models/nation.rb
@@ -19,7 +19,7 @@
 #  created_at      :datetime
 #  updated_at      :datetime
 #  tax_rate        :integer          not null
-#  environment     :integer
+#  environment     :integer          not null
 #
 
 class Nation < ActiveRecord::Base
