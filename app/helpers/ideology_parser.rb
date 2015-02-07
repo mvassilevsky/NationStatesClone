edc3518da99eabc2f24e69ee5cae73f3b4e12684
Zachary Perlmutter <zrp200@gmail.com>
2015-02-06 20:45:15 -0800

    Chained assignments

--- app/helpers/ideology_parser.rb
+++ app/helpers/ideology_parser.rb
@@ -1,19 +1,15 @@
 class IdeologyParser
 
   def self.questions
-    questions = []
-    questions[0] = "There should be a welfare state to take care of the worst-off."
-    questions[1] = "Democracy is a good political system."
-    questions
+   	[
+    		"There should be a welfare state to take care of the worst-off.",
+    		"Democracy is a good political system."
+	]
   end
 
   def initialize (ideology_params)
-    @ec_freedom = 0
-    @soc_freedom = 0
-    @pol_freedom = 0
-    @tax_rate = 0
-    @q1_response = ideology_params["q1"].to_i
-    @q2_response = ideology_params["q2"].to_i
+    @ec_freedom = @soc_freedom = @pol_freedom = @tax_rate = 0
+    @q1_response, @q2_response = ideology_params["q1"].to_i, ideology_params["q2"].to_i
   end
 
   def parse
