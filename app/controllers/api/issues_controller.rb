f10316fa9558bab2217a8571a8da03a2201b0dc8
Michael Vassilevsky <mvassilevsky@gmail.com>
2015-02-09 13:36:25 -0800

    added respond in issues controller

--- app/controllers/api/issues_controller.rb
+++ app/controllers/api/issues_controller.rb
@@ -16,6 +16,24 @@ module Api
 
     def respond
       @issue_option = IssueOption.find(params[:id])
+      new_ec_freedom = current_nation.ec_freedom + @issue_option.ec_freedom
+      new_soc_freedom = current_nation.soc_freedom + @issue_option.soc_freedom
+      new_pol_freedom = current_nation.pol_freedom + @issue_option.pol_freedom
+      new_tax_rate = current_nation.tax_rate + @issue_option.tax_rate
+      new_ecosystem = current_nation.ecosystem + @issue_option.ecosystem
+      nation_params = {
+        ec_freedom: new_ec_freedom,
+        soc_freedom: new_soc_freedom,
+        pol_freedom: new_pol_freedom,
+        tax_rate: new_tax_rate,
+        ecosystem: new_ecosystem
+      }
+      if current_nation.update_attributes(nation_params)
+        render :index
+      else
+        render json: current_nation.errors.full_messages,
+               status: :unprocessable_entity
+      end
     end
 
     private
