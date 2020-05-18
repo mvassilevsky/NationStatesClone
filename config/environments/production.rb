65387724cf94411af2995659eacc9d1aa52f44bd
Michael Vassilevsky <mvassilevsky@gmail.com>
2020-05-18 11:37:38 -0700

    Update logging for Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ NationStatesClone::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.logger = Logger.new(STDOUT)
+  config.log_level = :info
 end
